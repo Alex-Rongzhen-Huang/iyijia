5734fa80c296660f4bb58ff77291fc20d11c40dd
jihao <jacky.jihao@gmail.com>
2014-06-23 22:21:00 +0800

    figured out redirect to a certain path after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,10 @@ class ApplicationController < ActionController::Base
   def access_denied(exception)
     redirect_to new_admin_user_session_path, :alert => exception.message
   end
+
+  protected
+
+  def after_sign_in_path_for(resource)
+    root_path  # can change to customized path like user info stuff
+  end
 end
