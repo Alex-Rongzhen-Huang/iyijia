09aab555cd2b2c05992db9d4db6ad7e7e86f26e9
AlexHuang <alex.rongzhen.huang@gmail.com>
2014-09-15 16:15:57 +0800

    Add Hour adn Minutes to backup filename to distinguish frequent backup.

--- script/iyijia_maint.sh
+++ script/iyijia_maint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-BACKUP_DATE=`date -d today +'%Y-%m-%d'`
+BACKUP_DATE=`date -d today +'%Y-%m-%d_%H-%M'`
 echo "localhost:5432:iyijia_development:postgres:postgres">~/.pgpass
 chmod 600 ~/.pgpass
 pg_dump -i -h localhost -p 5432 -U postgres -F c -b -v -f "/home/hao/backup/${BACKUP_DATE}.sql" iyijia_development
