f8f30cdab61cec20a3a2809bc32c42375f784804
AlexHuang <alex.rongzhen.huang@gmail.com>
2014-09-17 09:59:29 +0800

    Change app path and backup path from hard code to input parameters.

--- script/iyijia_backup.sh
+++ script/iyijia_backup.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
+
+APP_PATH=$1
+BACKUP_PATH=$2
+
+usage()
+{
+    echo -e "\nUsage: iyijia_backup.sh [Application Path] [Backup Path]"
+    echo -e "Example: iyijia_backup.sh /home/hao/github/iyijia /home/hao/backup\n"
+}
+
+if [[ ! $APP_PATH || ! $BACKUP_PATH ]]; then
+  usage
+  exit 1
+fi
+
 BACKUP_DATE=`date -d today +'%Y-%m-%d_%H-%M'`
-echo "localhost:5432:iyijia_development:postgres:postgres">~/.pgpass
-chmod 600 ~/.pgpass
-pg_dump -i -h localhost -p 5432 -U postgres -F c -b -v -f "/home/hao/backup/${BACKUP_DATE}.sql" iyijia_development
-tar -cvf "/home/hao/backup/${BACKUP_DATE}.tar" /home/hao/github/iyijia/public/uploads 
+#echo "localhost:5432:iyijia_development:postgres:postgres">~/.pgpass
+#chmod 600 ~/.pgpass
+
+pg_dump -i -h localhost -p 5432 -U postgres -F c -b -v -f "${BACKUP_PATH}/${BACKUP_DATE}.sql" iyijia_development
+
+cd $APP_PATH/public
+tar -cvf "${BACKUP_PATH}/${BACKUP_DATE}.tar" uploads
--- script/iyijia_restore.sh
+++ script/iyijia_restore.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
-dropdb -p 5432 -U postgres iyijia_development
-createdb -p 5432 -U postgres iyijia_development
-pg_restore -i -h localhost -p 5432 -U postgres -d iyijia_development -v %1
+BACKUP_PATH=$1
+BACKUP_TIME=$2
+APP_PATH=$3
+
+usage()
+{
+    echo -e "\nUsage: iyijia_restore.sh [Application Path] [Backup Path]"
+    echo -e "Example: iyijia_restore.sh /home/hao/backup 2014-09-15_08_01 /home/hao/github/iyijia\n"
+}
+
+if [[ ! $APP_PATH || ! $BACKUP_PATH || ! $BACKUP_TIME ]]; then
+  usage
+  exit 1
+fi
+
+rm -rf  $APP_PATH/public/uploads
+tar -xvf $BACKUP_PATH/$BACKUP_TIME.tar -C $APP_PATH/public/
+
+echo "localhost:5432:iyijia_development:postgres:postgres">~/.pgpass
+chmod 600 ~/.pgpass
+pg_restore -i -h localhost -p 5432 -U postgres -c -d iyijia_development -v $BACKUP_PATH/$BACKUP_TIME.sql
